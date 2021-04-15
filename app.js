a71fe35df0b37ead89ee2ecf2f31ea20eb563100
AlexanderKuniansky <alik724@gmail.com>
2021-04-15 23:20:16 +0300

    Minor naming changes for consistency

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./db-pg-adapter/queries-data')
 const userDb = require('./db-pg-adapter/queries-user')
-const verifytoken = require('./authentication/verifytoken')
+const verifyToken = require('./authentication/verify-token')
 const app = express()
 const port = 3000
 
@@ -21,18 +21,18 @@ app.get('/', (request, response) => {
 })
 
 //Routing CRUD for categories, verifies token then sends to db adapter with pg queries
-app.get('/categories', verifytoken, db.getCategories)
-app.get('/categories/:id', verifytoken, db.getCategoryById)
-app.post('/categories', verifytoken, db.createCategory)
-app.put('/categories/:id', verifytoken, db.updateCategory)
-app.delete('/categories/:id', verifytoken, db.deleteCategory)
+app.get('/categories', verifyToken, db.getCategories)
+app.get('/categories/:id', verifyToken, db.getCategoryById)
+app.post('/categories', verifyToken, db.createCategory)
+app.put('/categories/:id', verifyToken, db.updateCategory)
+app.delete('/categories/:id', verifyToken, db.deleteCategory)
 
 //Routing CRUD for products
-app.get('/products', verifytoken, db.getProducts)
-app.get('/products/:id', verifytoken, db.getProductById)
-app.post('/products', verifytoken, db.createProduct)
-app.put('/products/:id', verifytoken, db.updateProduct)
-app.delete('/products/:id', verifytoken, db.deleteProduct)
+app.get('/products', verifyToken, db.getProducts)
+app.get('/products/:id', verifyToken, db.getProductById)
+app.post('/products', verifyToken, db.createProduct)
+app.put('/products/:id', verifyToken, db.updateProduct)
+app.delete('/products/:id', verifyToken, db.deleteProduct)
 
 //Routes for registering and user login aka providing token
 app.post('/register', userDb.createUser)
